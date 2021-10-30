9978076f14b8a1bcb6e5947d14d1f01148621f33
honeycoded / <abbasab13@outlook.com>
2021-10-30 09:10:24 +0300

    First node and express project

---
+++ meadowlark.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+const handlebars = require("express3-handlebars").create({
+  defaultLayout: "main",
+});
+
+app.engine("handlebars", handlebars.engine);
+app.set("view engine", "handlebars");
+
+app.set("port", process.env.PORT || 3000);
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+  res.render("home.handlebars");
+});
+
+const names = ["abbas", "ali", "mhmd", "hussein"];
+app.get("/about", (req, res) => {
+  let randomName = names[Math.floor(Math.random() * names.length)];
+  res.render("about", { names: "hello " + randomName });
+});
+
+app.use((req, res) => {
+  res.status(404);
+  res.render("404");
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500);
+  res.render("500");
+});
+
+app.listen(app.get("port"), () => {
+  console.log("server started on localhost:" + app.get("port"));
+});
